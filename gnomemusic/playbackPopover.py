928ce13d951fa65a331399c0e9d5f881d0362dfa
Jordana Luft <jordana.sluft@gmail.com>
2016-02-10 16:55:21 -0200

    Playback: Implemented update_popover
    This method will be used to update_popover.
    It's conected to signal 'playlist-item-changed'

--- gnomemusic/playbackPopover.py
+++ gnomemusic/playbackPopover.py
@@ -13,11 +13,19 @@ class PlaybackPopover:
         self.popover = self._ui.get_object('playbackPopover')
         self.popover.set_relative_to(self.player.nowplaying_button)
 
+        self.label = Gtk.Label()
+        self.popover.add(self.label)
+
+        self.player.connect('playlist-item-changed', self.update_popover)
+
     def toggle_popover(self):
         if self.popover.get_visible():
             self.popover.hide()
         else:
-            self.popover.show()
+            self.popover.show_all()
+
+    def update_popover(self, player, playlist, current_track):
+        self.label.set_label(self.player.playlistType)
 
 
 class Song:
